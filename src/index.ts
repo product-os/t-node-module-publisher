056eb0857e397284b0035e66c9ab30084479675e
Dan Goodman <danthegoodman@icloud.com>
2021-12-14 14:01:38 -0500

    Push secrets
    Change-type: patch

--- src/index.ts
+++ src/index.ts
@@ -30,8 +30,12 @@ const run = async () => {
 		await zx.$`cp -r ${path.join(backflowPath, 'artifact')} /tmp`
 
 		// Write the decrypted secret
-		if (input.decryptedTransformerSecrets && input.decryptedTransformerSecrets.buildSecrets && input.decryptedTransformerSecrets.buildSecrets['NPM_TOKEN']) {
-			await fs.writeFile(path.join(os.homedir(), '.npmrc'), `//registry.npmjs.org/:_authToken=${input.decryptedTransformerSecrets.buildSecrets['NPM_TOKEN']}`)
+		const secrets = {
+			...input.decryptedSecrets?.buildSecrets,
+			...input.decryptedTransformerSecrets?.buildSecrets,
+		};
+		if (secrets['NPM_TOKEN']) {
+			await fs.writeFile(path.join(os.homedir(), '.npmrc'), `//registry.npmjs.org/:_authToken=${secrets['NPM_TOKEN']}`)
 		} else {
 			console.error('[PUBLISHER] Failed to find NPM_TOKEN secret, exiting!')
 			process.exit(1)
